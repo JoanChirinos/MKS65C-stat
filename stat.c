1c3c23487f862439c8611362047119b51ee9f2ea
camillac <ccheng6@stuy.edu>
2018-10-30 20:27:33 -0400

    readable size + mode

--- stat.c
+++ stat.c
@@ -13,4 +13,19 @@ int main() {
   char date[36];
   strftime(date, 36, "%c", localtime(&(s->st_atime)));
   printf("time of last access: %s\n", date);
+
+  printf("\nReadable size + mode\n");
+  printf("file size: %ld B\n", s->st_size);
+  printf("file mode: ");
+  printf( (S_ISDIR(s->st_mode)) ? "d" : "-");
+  printf( (s->st_mode & S_IRUSR) ? "r" : "-");
+  printf( (s->st_mode & S_IWUSR) ? "w" : "-");
+  printf( (s->st_mode & S_IXUSR) ? "x" : "-");
+  printf( (s->st_mode & S_IRGRP) ? "r" : "-");
+  printf( (s->st_mode & S_IWGRP) ? "w" : "-");
+  printf( (s->st_mode & S_IXGRP) ? "x" : "-");
+  printf( (s->st_mode & S_IROTH) ? "r" : "-");
+  printf( (s->st_mode & S_IWOTH) ? "w" : "-");
+  printf( (s->st_mode & S_IXOTH) ? "x" : "-");
+  printf("\n");
 }
